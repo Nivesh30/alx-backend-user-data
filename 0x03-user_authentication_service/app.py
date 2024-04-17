5e72428a656b23499a82da7d2f4ffcd2fc6d801c
Nivesh <105005447+Nivesh30@users.noreply.github.com>
2024-04-17 18:29:13 +0200

    local

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,122 +1,117 @@
 #!/usr/bin/env python3
 """
-Flask app
+A User authentication service application
 """
+from flask import Flask
+from flask import (
+    abort, jsonify,
+    request, make_response, redirect
+)
+
 from auth import Auth
-from flask import Flask, jsonify, request, abort, redirect
 
 AUTH = Auth()
+
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def hello() -> str:
-    """GET route index
-
-    Returns:
-        str: json {'message': 'Bienvenue'}
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized error handler
     """
-    return jsonify({"message": "Bienvenue"}), 200
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def unauthorized(error) -> str:
+    """ Forbidden error handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.route("/")
+def home() -> str:
+    """
+    Home route
+    """
+    return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def user() -> str:
-    """POST route for user register
-
-    Returns:
-        str: messege
+def users() -> str:
     """
-    email = request.form.get('email')
-    password = request.form.get('password')
+    Register a new user route
+    """
+    email, password = request.form.get('email'), request.form.get('password')
     try:
-        AUTH.register_user(email, password)
-        return jsonify({"email": f"{email}", "message": "user created"}), 200
-    except Exception:
-        return jsonify({"messege": "email already registered"}), 400
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": "%s" % email, "message": "user created"})
 
 
-@app.route('/sessions', methods=['POST'], strict_slashes=False)
-def login() -> str:
-    """login
-
-    Returns:
-        str: messege
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Login a valid user and set a session id
     """
-    email = request.form.get('email')
-    password = request.form.get('password')
-    valid_login = AUTH.valid_login(email, password)
-    if not valid_login:
-        abort(401)
-    session_id = AUTH.create_session(email)
-    response = jsonify({"email": f"{email}", "message": "logged in"})
-    response.set_cookie('session_id', session_id)
-    return response
+    email, password = request.form.get('email'), request.form.get('password')
+    if AUTH.valid_login(email=email, password=password):
+        res = make_response(
+            jsonify({"email": "%s" % email, "message": "logged in"}))
+        res.set_cookie("session_id", AUTH.create_session(email))
+        return res
+    abort(401)
 
 
-@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout() -> str:
-    """ logout
-
-    Return:
-       str: message
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    """Logs out a login user
     """
-    session_id = request.cookies.get('session_id')
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
-    if user:
+    if user is not None:
         AUTH.destroy_session(user.id)
-        return redirect('/')
-    else:
-        abort(403)
+        return redirect("/")
+    abort(403)
 
 
-@app.route('/profile', methods=['GET'], strict_slashes=False)
-def profile() -> str:
-    """profile
-
-    Return:
-       str: message
+@app.route("/profile", strict_slashes=False)
+def profile():
+    """Gets and returns json data of user profile
     """
-    session_id = request.cookies.get('session_id')
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
-        return jsonify({"email": user.email}), 200
-    else:
-        abort(403)
+        return jsonify({"email": user.email})
+    abort(403)
 
 
-@app.route('/reset_password', methods=['POST'], strict_slashes=False)
-def get_reset_password_token() -> str:
-    """get_reset_password
-
-    Return:
-       str: message
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token():
+    """Reset password route
     """
-    email = request.form.get('email')
-    user = AUTH.create_session(email)
-    if not user:
-        abort(403)
-    else:
-        token = AUTH.get_reset_password_token(email)
-        return jsonify({"email": f"{email}", "reset_token": f"{token}"})
-
-
-@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password() -> str:
-    """update_password
-
-    Return:
-       str: message
-    """
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_psw = request.form.get('new_password')
+    email = request.form.get("email")
     try:
-        AUTH.update_password(reset_token, new_psw)
-        return jsonify({"email": f"{email}",
-                        "message": "Password updated"}), 200
-    except Exception:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
         abort(403)
+    return jsonify({"email": "%s" % email, "reset_token": "%s" % reset_token})
 
 
-if __name__ == "__main__":
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password():
+    """Update password route
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": "%s" % email, "message": "Password updated"})
+    except Exception:
+        pass
+    abort(403)
+
+
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,59 +1,107 @@
 #!/usr/bin/env python3
 """
-Main Module
+End to end integration testing
 """
-from db import DB
-from user import User
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+import requests
+
+domain: str = "http://0.0.0.0:5000/%s"
 
 
 def register_user(email: str, password: str) -> None:
-    """register_user"""
-    assert True
-    return
+    """Test registeration endpoint
+    """
+    url = domain % "users"
+    payload = [("email", email), ("password", password)]
+    expected = {"email": email, "message": "user created"}
+    res = requests.post(url, data=payload)
+    assert res.status_code == 200
+    assert res.json() == expected
+
+    expected = {"message": "email already registered"}
+    res = requests.post(url, data=payload)
+    assert res.status_code == 400
+    assert res.json() == expected
 
 
 def log_in_wrong_password(email: str, password: str) -> None:
-    """log_in_wrong_password"""
-    assert True
-    return
-
-
-def log_in(email: str, password: str) -> str:
-    """log_in"""
-    assert True
-    return ("")
+    """Test login endpoint with wrong password
+    """
+    url = domain % "sessions"
+    payload = [("email", email), ("password", password)]
+    expected = {"error": "Unauthorized"}
+    res = requests.post(url, data=payload)
+    assert res.status_code == 401
+    assert res.json() == expected
+    assert res.cookies == {}
+    return res.cookies.get("session_id", None)
 
 
 def profile_unlogged() -> None:
-    """profile_unlogged"""
-    assert True
-    return
+    """Test profile route with unlogged user
+    """
+    url = domain % "profile"
+    expected = {"error": "Forbidden"}
+    res = requests.get(url, cookies=dict())
+    assert res.status_code == 403
+    assert res.json() == expected
+
+
+def log_in(email: str, password: str) -> str:
+    """Test login with valid credentials
+    """
+    url = domain % "sessions"
+    payload = [("email", email), ("password", password)]
+    expected = {"email": "%s" % email, "message": "logged in"}
+    res = requests.post(url, data=payload)
+    session_id = res.cookies.get("session_id", None)
+    assert res.status_code == 200
+    assert res.json() == expected
+    assert session_id is not None
+    return session_id
 
 
 def profile_logged(session_id: str) -> None:
-    """profile_logged"""
-    assert True
-    return
+    """Test profile route for a valid logged in user
+    """
+    url = domain % "profile"
+    res = requests.get(url, cookies=dict(session_id=session_id))
+    assert res.status_code == 200
+    assert res.json().get("email") is not None
 
 
 def log_out(session_id: str) -> None:
-    """log_out"""
-    assert True
-    return
+    """Test logout call
+    """
+    url = domain % "sessions"
+    res = requests.delete(url, cookies=dict(session_id=session_id))
+    print(res.status_code)
+    assert res.status_code == 200
+    assert res.json() == {"message": "Bienvenue"}
 
 
 def reset_password_token(email: str) -> str:
-    """reset_password_token"""
-    assert True
-    return ("")
+    """Test reset token route
+    """
+    url = domain % "reset_password"
+    res = requests.post(url, data={"email": email})
+    assert res.status_code == 200
+    reset_token = res.json().get("reset_token")
+    assert type(reset_token) == str
+    assert len(reset_token) > 0
+    return reset_token
 
 
-def update_password(reset_token: str, new_password: str) -> None:
-    """update_password"""
-    assert True
-    return
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """Test passward update route
+    """
+    url = domain % "reset_password"
+    payload = [
+        ("email", email),
+        ("reset_token", reset_token), ("new_password", new_password)]
+    expected = {"email": "%s" % email, "message": "Password updated"}
+    res = requests.put(url, data=payload)
+    assert res.status_code == 200
+    assert res.json() == expected
 
 
 EMAIL = "guillaume@holberton.io"
@@ -70,5 +118,5 @@ if __name__ == "__main__":
     profile_logged(session_id)
     log_out(session_id)
     reset_token = reset_password_token(EMAIL)
-    update_password(EMAIL, reset_token)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
     log_in(EMAIL, NEW_PASSWD)
